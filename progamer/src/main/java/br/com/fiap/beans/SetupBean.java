041bc5c767aa2d91e5cbb0d6d1f3e65ffe98cd97
Joao Carlos <joaocarloslima@me.com>
2021-03-30 13:14:18 -0300

    inicio do projeto progamer

---
+++ progamer/src/main/java/br/com/fiap/beans/SetupBean.java
@@ -0,0 +1,12 @@
+package br.com.fiap.beans;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class SetupBean {
+	
+	public void executar() {
+		System.out.println("acionando o bean");
+	}
+
+}
